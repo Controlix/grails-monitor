0a475af0519fea151d198b87286103e2d62c9852
Marc Bogaerts <marcbogaerts@telenet.be>
2018-04-30 13:11:44 +0200

    Recreate actors at starup

--- grails-app/services/grails/monitor/MonitorService.groovy
+++ grails-app/services/grails/monitor/MonitorService.groovy
@@ -2,6 +2,8 @@ package grails.monitor
 
 import static conf.SpringExtension.SpringExtProvider
 
+import javax.annotation.PostConstruct
+
 import akka.actor.ActorSystem
 import akka.actor.Props
 import grails.events.Event
@@ -10,28 +12,38 @@ import grails.events.annotation.Subscriber
 class MonitorService {
 
 	ActorSystem actorSystem
+	ServiceMonitorService serviceMonitorService
 
 	def monitors = [:]
+	
+	@PostConstruct
+	def init() {
+		serviceMonitorService.list().each { startMonitoring(it) }
+	}
 
 	@Subscriber
 	def onSave(ServiceMonitor serviceMonitor) {
 		if (! monitors[serviceMonitor.id]) {
-			def requestId = UUID.randomUUID().toString()
-
-			// describe the properties of the desired actor
-			def actorType = "MonitorActor"
-			Props props = SpringExtProvider.get(actorSystem).props(actorType)
-
-			// ask akka to create the actor
-			// use unique actor name because this will be an ephemeral, stateless actor
-			// managed as a prototype bean in the spring context
-			def actorName = "${actorType}-${requestId}"
-			
-			monitors[serviceMonitor.id] = actorSystem.actorOf(props, actorName)
+			startMonitoring(serviceMonitor)
 		}
 
 		log.info "Saved {}", serviceMonitor
 		log.info "All monitors {}", monitors
+	}
+
+	private startMonitoring(ServiceMonitor serviceMonitor) {
+		def requestId = UUID.randomUUID().toString()
+
+		// describe the properties of the desired actor
+		def actorType = "MonitorActor"
+		Props props = SpringExtProvider.get(actorSystem).props(actorType)
+
+		// ask akka to create the actor
+		// use unique actor name because this will be an ephemeral, stateless actor
+		// managed as a prototype bean in the spring context
+		def actorName = "${actorType}-${requestId}"
+
+		monitors[serviceMonitor.id] = actorSystem.actorOf(props, actorName)
 	}
 
 	@Subscriber
--- grails-app/utils/conf/SpringActorProducer.groovy
+++ grails-app/utils/conf/SpringActorProducer.groovy
@@ -20,13 +20,13 @@ public class SpringActorProducer implements IndirectActorProducer {
 
   @Override
   public Actor produce() {
-    println "produce() called for actorBeanName: ${actorBeanName}"
+    log.debug "produce() called for actorBeanName: {}", actorBeanName
     return (Actor) applicationContext.getBean(actorBeanName);
   }
 
   @Override
   public Class<? extends Actor> actorClass() {
-    println "actorClass() called for actorBeanName: ${actorBeanName}"
+    log.debug "actorClass() called for actorBeanName: {}", actorBeanName
     return (Class<? extends Actor>) applicationContext.getType(actorBeanName);
   }
 }
